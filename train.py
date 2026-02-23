ef89f5c8086b59fdf755a52226c6a5bf7006a4c6
yosefnuragaw <yosefnuragaw@gmail.com>
2026-02-24 00:51:56 +0700

    feat: conf

--- train.py
+++ train.py
@@ -147,7 +147,7 @@ if __name__ == "__main__":
         lr_scheduler_type=script_args.lr_scheduler_type,
         warmup_steps=script_args.warmup_steps,
         optim=script_args.optimizer_type,
-        bf16=True,
+        bf16=False,
         remove_unused_columns=False,
         max_prompt_length=script_args.max_prompt_length,
         max_length=script_args.max_length,

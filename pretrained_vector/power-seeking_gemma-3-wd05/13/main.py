5feb8f7fbac9057ddd8bf0d18faa83ed6c48122d
yosefnuragaw <yosefnuragaw@gmail.com>
2026-02-23 23:38:25 +0700

    feat: vecs

--- pretrained_vector/power-seeking_gemma-3-wd05/13/main.py
+++ pretrained_vector/power-seeking_gemma-3-wd05/13/main.py
@@ -25,7 +25,7 @@ def download_steering_vectors_flat(entity, project, artifact_base_name, versions
 
 ENTITY = "yosefnuragaw" 
 PROJECT = "BiPO-Gemma-3-Power-Seeking"
-ARTIFACT_NAME = "power-seeking-Layers_13-jqows3tw_steering-vec-layer13"
+ARTIFACT_NAME = "power-seeking-Layers_13-k8lfhcrt_steering-vec-layer13"
 
 versions_to_get = list(range(20)) 
 

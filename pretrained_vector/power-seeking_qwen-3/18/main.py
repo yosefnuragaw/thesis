84da7d517343158ec789e6e357ea1ffe907f0999
yosefnuragaw <yosefnuragaw@gmail.com>
2026-02-25 15:16:10 +0700

    feat: confs lay 11

--- pretrained_vector/power-seeking_qwen-3/18/main.py
+++ pretrained_vector/power-seeking_qwen-3/18/main.py
@@ -25,9 +25,9 @@ def download_steering_vectors_flat(entity, project, artifact_base_name, versions
 
 ENTITY = "yosefnuragaw" 
 PROJECT = "BiPO-New-Gemma-3-Power-Seeking"
-ARTIFACT_NAME = "power-seeking-Layers_18-u7xz36zt_steering-vec-layer18"
+ARTIFACT_NAME = "power-seeking-Layers_18-a12ajum7_steering-vec-layer18"
 
-versions_to_get = list(range(50)) 
+versions_to_get = list(range(100)) 
 
 paths = download_steering_vectors_flat(ENTITY, PROJECT, ARTIFACT_NAME, versions_to_get)
 

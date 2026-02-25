38f47e8159acdcee60019655b0834b4b1e3b9d39
yosefnuragaw <yosefnuragaw@gmail.com>
2026-02-25 17:04:20 +0700

    feat: confs lay 11

--- evaluation.py
+++ evaluation.py
@@ -48,7 +48,7 @@ def init_model(
         model_name,
         low_cpu_mem_usage=True,
         trust_remote_code=True,
-        torch_dtype=torch.float16
+        torch_dtype="auto"
     )
     model.warnings_issued = {}
     model.to("cuda" if torch.cuda.is_available() else "cpu")

6954eaa72975f6983f578120e5d593ae2bcf301d
yosefnuragaw <yosefnuragaw@gmail.com>
2026-02-27 12:00:44 +0700

    feat: add llm as judge

--- evaluation_llm.py
+++ evaluation_llm.py
@@ -93,7 +93,7 @@ def main(baseline: bool, args: ScriptArguments) -> None:
         if not dataset:
             continue
 
-        print(f"\n================ Multiplier {mul} ================")
+        print(f"\n[Multiplier:] {mul} \n")
         
         coherence_eval_inputs = [
             EvalInput(inputs=[{"question": row['question']}], output={"answer": row['answer']}) 

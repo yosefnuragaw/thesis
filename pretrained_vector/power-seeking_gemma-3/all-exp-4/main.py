802ddd377fb903b0b6c37c3a26832bb49d0ca1d7
yosefnuragaw <yosefnuragaw@gmail.com>
2026-03-02 19:13:11 +0700

    feat: vecs conf 4

--- pretrained_vector/power-seeking_gemma-3/all-exp-4/main.py
+++ pretrained_vector/power-seeking_gemma-3/all-exp-4/main.py
@@ -13,7 +13,7 @@ def download_full_bipo_history():
     project = "BiPO-Gemma-3-1b-Power-Seeking"
 
     # The specific base path identified from your terminal success
-    base_name = "power-seeking-power-seeking-all-exp-4-k5ivy2h6_steering-vec-layer"
+    base_name = "power-seeking-power-seeking-all-exp-4-trl5caqt_steering-vec-layer"
     
     layers = range(26)  # 0 to 25
     versions = range(20) # v0 to v19

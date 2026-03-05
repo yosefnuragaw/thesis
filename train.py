0ee47fd9a7ba553438b885700b56ceca7592e109
yosefnuragaw <yosefnuragaw@gmail.com>
2026-03-05 21:17:57 +0700

    feat: vecs

--- train.py
+++ train.py
@@ -49,6 +49,8 @@ class ScriptArguments:
         default_factory=lambda: list(range(26)), 
         metadata={"help": "the layer the steering vector extracted from"}
     )
+    num_layer: Optional[int] = field(default=26, metadata={"help": "total model layer"})
+
     report_to: Optional[str] = field(default="wandb", metadata={"help": "integration to report to"})
     ignore_bias_buffers: Optional[bool] = field(default=False, metadata={"help": "fix for DDP issues"})
 
@@ -173,7 +175,7 @@ if __name__ == "__main__":
             filter_step=script_args.filter_step,
             experiment_pipeline = script_args.experiment_pipeline,
             masking_type = script_args.masking_type,
-            num_layer = script_args.total_layer
+            num_layer = script_args.num_layer
         )
 
         if script_args.quantile_scheduler:

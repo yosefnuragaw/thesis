f4c0c1e02be12bd1c0cd3fc473a227de622b81e1
yosefnuragaw <yosefnuragaw@gmail.com>
2026-03-09 18:19:28 +0700

    feat: add llama 3.1

--- pretrained_vector/power-seeking/llama-3/16/main.py
+++ pretrained_vector/power-seeking/llama-3/16/main.py
@@ -12,7 +12,7 @@ def download_best_bipo_step():
     
     entity = "yosefnuragaw"
     project = "Power-Seeking"
-    run_id = "rn3nixzn" 
+    run_id = "2j15bxr9" 
     
     print("Fetching run history (Max 10,000 rows)...")
     run = api.run(f"{entity}/{project}/{run_id}")
--- pretrained_vector/power-seeking/llama-3/all/main.py
+++ pretrained_vector/power-seeking/llama-3/all/main.py
@@ -12,7 +12,7 @@ def download_best_bipo_step():
     
     entity = "yosefnuragaw"
     project = "Power-Seeking"
-    run_id = "rdm09hd7" 
+    run_id = "3wjar303" 
     
     print("Fetching run history (Max 10,000 rows)...")
     run = api.run(f"{entity}/{project}/{run_id}")

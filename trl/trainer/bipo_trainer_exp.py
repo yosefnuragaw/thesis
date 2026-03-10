0db5e465f070f442b86962a5d99b8c58ad452488
yosefnuragaw <yosefnuragaw@gmail.com>
2026-03-10 13:54:17 +0700

    feat: add llama3

--- trl/trainer/bipo_trainer_exp.py
+++ trl/trainer/bipo_trainer_exp.py
@@ -100,44 +100,44 @@ class BiPOTrainerEXP(BiPOTrainer):
         return loss
 
      # EXP 11: Fisher Layer selection
-    @override
-    def training_step(self, model, inputs, num_items_in_batch=None):
-        is_filter_step = (self.state.global_step % self.filter_step == 0)
+    # @override
+    # def training_step(self, model, inputs, num_items_in_batch=None):
+    #     is_filter_step = (self.state.global_step % self.filter_step == 0)
 
-        # 1. Initial mapping
-        if self.state.global_step == 0:  
-            vec_idx = 0
-            for name, param in model.named_parameters():
-                if "vec" in name:
-                    self.idx_param[vec_idx] = param
-                    vec_idx += 1
+    #     # 1. Initial mapping
+    #     if self.state.global_step == 0:  
+    #         vec_idx = 0
+    #         for name, param in model.named_parameters():
+    #             if "vec" in name:
+    #                 self.idx_param[vec_idx] = param
+    #                 vec_idx += 1
 
-        if is_filter_step:
-            for param in self.idx_param.values():
-                param.requires_grad = True
+    #     if is_filter_step:
+    #         for param in self.idx_param.values():
+    #             param.requires_grad = True
                 
-        # 3. Forward and Backward pass
-        loss = super().training_step(model, inputs, num_items_in_batch)
+    #     # 3. Forward and Backward pass
+    #     loss = super().training_step(model, inputs, num_items_in_batch)
 
-        if is_filter_step:
-            with torch.no_grad():
-                for idx, param in self.idx_param.items():
-                    if param.grad is not None:
-                        self.layer_weight[idx] = param.grad.pow(2).mean()
+    #     if is_filter_step:
+    #         with torch.no_grad():
+    #             for idx, param in self.idx_param.items():
+    #                 if param.grad is not None:
+    #                     self.layer_weight[idx] = param.grad.pow(2).mean()
 
-                self.quantile_threshold = getattr(self.state, "custom_quantile_threshold", self.quantile_threshold)
-                threshold = torch.quantile(self.layer_weight, 1 - self.quantile_threshold) 
-                hard_mask = self.layer_weight >= threshold
+    #             self.quantile_threshold = getattr(self.state, "custom_quantile_threshold", self.quantile_threshold)
+    #             threshold = torch.quantile(self.layer_weight, 1 - self.quantile_threshold) 
+    #             hard_mask = self.layer_weight >= threshold
 
-                hard_mask_list = hard_mask.tolist()
-                for idx, param in self.idx_param.items():
-                    is_learning = hard_mask_list[idx]
-                    param.requires_grad = is_learning
+    #             hard_mask_list = hard_mask.tolist()
+    #             for idx, param in self.idx_param.items():
+    #                 is_learning = hard_mask_list[idx]
+    #                 param.requires_grad = is_learning
                     
-                    if not is_learning and param.grad is not None:
-                        param.grad.zero_()
+    #                 if not is_learning and param.grad is not None:
+    #                     param.grad.zero_()
 
-        return loss
+    #     return loss
     
     # https://arxiv.org/pdf/2503.11164
     # Layer sensitivity analysis using Fisher Information Matrix to Approximate Hessian matrix trace

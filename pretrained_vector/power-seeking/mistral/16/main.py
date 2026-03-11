1466302f29fd822360cc14e438bd6b75b0f7e7b7
yosefnuragaw <yosefnuragaw@gmail.com>
2026-03-11 15:30:22 +0700

    feat: add mistral 16

--- pretrained_vector/power-seeking/mistral/16/main.py
+++ pretrained_vector/power-seeking/mistral/16/main.py
@@ -12,7 +12,7 @@ def download_best_bipo_step():
     
     entity = "yosefnuragaw"
     project = "Power-Seeking"
-    run_id = "km2xp9p8" 
+    run_id = "vh5m4k32" 
     
     print("Fetching run history (Max 10,000 rows)...")
     run = api.run(f"{entity}/{project}/{run_id}")

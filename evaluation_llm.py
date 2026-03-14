e969b89b78b23a40db7830d898f13b6b77a33b92
yosefnuragaw <yosefnuragaw@gmail.com>
2026-03-14 21:19:02 +0700

    feat: add exp

--- evaluation_llm.py
+++ evaluation_llm.py
@@ -164,7 +164,7 @@ def main(baseline: bool, args: ScriptArguments) -> None:
         c_score = evaluate_batch(judge_pipe, tokenizer, coherence_factory, coherence_raw_prompts, "Utility")
         coherence_likert[mul] = c_score
         if mul == 0:
-            coherence_path = f"reasoning/{args.behavior}/baseline/reasoning_utility_mul_{mul}.csv"
+            coherence_path = f"reasoning/{args.behavior}/{args.model_name_or_path.replace('/', '_')}/reasoning_utility_mul_{mul}.csv"
         else:
             coherence_path = f"reasoning/{args.behavior}/{args.id}/reasoning_utility_mul_{mul}.csv"
     
@@ -173,7 +173,7 @@ def main(baseline: bool, args: ScriptArguments) -> None:
         b_score = evaluate_batch(judge_pipe, tokenizer, behavior_factory, behavior_raw_prompts, "Behavior")
         accuracy_likert[mul] = b_score
         if mul == 0:
-            behavior_path = f"reasoning/{args.behavior}/baseline/reasoning_behavior_mul_{mul}.csv"
+            behavior_path = f"reasoning/{args.behavior}/{args.model_name_or_path.replace('/', '_')}/reasoning_behavior_mul_{mul}.csv"
         else:
             behavior_path = f"reasoning/{args.behavior}/{args.id}/reasoning_behavior_mul_{mul}.csv"
 

c4894553d23a468c38337f07581f1684577ad201
yosefnuragaw <yosefnuragaw@gmail.com>
2026-03-16 02:36:22 +0700

    feat: add exponen exp

--- models/model.py
+++ models/model.py
@@ -40,7 +40,7 @@ class BlockWrapper(torch.nn.Module):
         super().__init__()
         self.multiplier = 1.0
         self.block = block
-        self.k1 = 0.3
+        self.k1 = 0.1
         try:
             ref_param = next(block.parameters())
             self.init_dtype = ref_param.dtype

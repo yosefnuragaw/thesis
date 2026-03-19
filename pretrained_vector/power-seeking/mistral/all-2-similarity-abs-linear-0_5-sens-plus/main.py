ef90cb85c1097f14c4250a797c1241fad9efaf88
yosefnuragaw <yosefnuragaw@gmail.com>
2026-03-20 03:14:22 +0700

    feat: add all-2 mistral

--- pretrained_vector/power-seeking/mistral/all-2-similarity-abs-linear-0_5-sens-plus/main.py
+++ pretrained_vector/power-seeking/mistral/all-2-similarity-abs-linear-0_5-sens-plus/main.py
@@ -12,7 +12,7 @@ def download_best_bipo_step():
     
     entity = "yosefnuragaw"
     project = "Final-Power-Seeking"
-    run_id = "duwi5af6" 
+    run_id = "np5hexvh" 
     
     print("Fetching run history (Max 10,000 rows)...")
     run = api.run(f"{entity}/{project}/{run_id}")
--- pretrained_vector/power-seeking/mistral/all-2-similarity-abs-linear-0_5/main.py
+++ pretrained_vector/power-seeking/mistral/all-2-similarity-abs-linear-0_5/main.py
@@ -12,7 +12,7 @@ def download_best_bipo_step():
     
     entity = "yosefnuragaw"
     project = "Final-Power-Seeking"
-    run_id = "ca5bfwm2" 
+    run_id = "gu5f7k1g" 
     
     print("Fetching run history (Max 10,000 rows)...")
     run = api.run(f"{entity}/{project}/{run_id}")

72cde668c078df1f1a1cfd9170c62368455dc9c3
yosefnuragaw <yosefnuragaw@gmail.com>
2026-03-22 02:26:16 +0700

    feat: add pt llama all-3

--- models/model.py
+++ models/model.py
@@ -80,10 +80,11 @@ class BlockWrapper(torch.nn.Module):
             abs_exp_sim = torch.exp(cos_sim.abs())
             linear_abs_sim =1 + self.k1*(2*cos_sim.abs()-1)
             linear_abs_sim_sens =1 + self.k1*(1-cos_sim.abs()*2)
-            lass = 1 + 0.5 (1-cos_sim.abs() *2)
-            ldss = 1 + 0.5 (cos_dis - 0.05)
-            linear_dis_sim_sens =1 + self.k1*((1-cos_sim)*2)
-            print(f'{self.block.__class__.__name__} cosine_distance: {cos_dis.mean().item()} | Scale lass {lass.mean().item()} | Scale ldss {ldss.mean().item()}')
+            lass = 1 + 0.5 * (1-cos_sim.abs() * 2)
+            lds = 1 + 0.5 * (cos_dis - 0.05)
+            ldi = 1 + 0.5 * (0.05 - cos_dis)
+
+            print(f'{self.block.__class__.__name__} cosine_distance: {cos_dis.mean().item()} | Scale lass {lass.mean().item()} | Scale ldss {lds.mean().item()} | Scale ldi {ldi.mean().item()}')
             
         mask = self.multiplier 
         if self.gate_mask:

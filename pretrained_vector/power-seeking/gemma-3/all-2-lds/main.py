394d2315a42bf29e14a713c8f49889410310bf06
yosefnuragaw <yosefnuragaw@gmail.com>
2026-03-22 04:08:11 +0700

    feat: add gemma ldi

--- pretrained_vector/power-seeking/gemma-3/all-2-lds/main.py
+++ pretrained_vector/power-seeking/gemma-3/all-2-lds/main.py
@@ -12,7 +12,7 @@ def download_best_bipo_step():
     
     entity = "yosefnuragaw"
     project = "Final-Power-Seeking"
-    run_id = "6yvq9ihh" 
+    run_id = "yoyxm7ch" 
     
     print("Fetching run history (Max 10,000 rows)...")
     run = api.run(f"{entity}/{project}/{run_id}")

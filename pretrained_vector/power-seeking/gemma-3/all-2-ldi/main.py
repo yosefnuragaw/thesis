aba9633c70c841c24fddc1edfc1d7dea841ce0e7
yosefnuragaw <yosefnuragaw@gmail.com>
2026-03-22 14:46:34 +0700

    feat: add ldi lds

--- pretrained_vector/power-seeking/gemma-3/all-2-ldi/main.py
+++ pretrained_vector/power-seeking/gemma-3/all-2-ldi/main.py
@@ -12,7 +12,7 @@ def download_best_bipo_step():
     
     entity = "yosefnuragaw"
     project = "Final-Power-Seeking"
-    run_id = "tpkuakmh" 
+    run_id = "h6y96rmg" 
     
     print("Fetching run history (Max 10,000 rows)...")
     run = api.run(f"{entity}/{project}/{run_id}")
--- pretrained_vector/power-seeking/gemma-3/all-2-lds/main.py
+++ pretrained_vector/power-seeking/gemma-3/all-2-lds/main.py
@@ -12,7 +12,7 @@ def download_best_bipo_step():
     
     entity = "yosefnuragaw"
     project = "Final-Power-Seeking"
-    run_id = "yoyxm7ch" 
+    run_id = "agrgzv1s" 
     
     print("Fetching run history (Max 10,000 rows)...")
     run = api.run(f"{entity}/{project}/{run_id}")

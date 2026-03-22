2f10df54492fea8b9b5b8bc65dd3c07c3451c027
yosefnuragaw <yosefnuragaw@gmail.com>
2026-03-22 18:29:34 +0700

    feat: add lds 0.75

--- pretrained_vector/power-seeking/gemma-3/all-2-llds-0_5/main.py
+++ pretrained_vector/power-seeking/gemma-3/all-2-llds-0_5/main.py
@@ -12,7 +12,7 @@ def download_best_bipo_step():
     
     entity = "yosefnuragaw"
     project = "Final-Power-Seeking"
-    run_id = "da8sst41" 
+    run_id = "12zf10uf" 
     
     print("Fetching run history (Max 10,000 rows)...")
     run = api.run(f"{entity}/{project}/{run_id}")
@@ -59,21 +59,21 @@ def download_best_bipo_step():
     print(f"-------------------------\n")
 
     # 6. Download the artifacts
-    # base_name = f"power-seeking-gemma3-1b-all-2-llds-0_5-{run_id}_steering-vec-layer"
-    # layers = range(32)
+    base_name = f"power-seeking-gemma3-1b-all-2-llds-0_5-{run_id}_steering-vec-layer"
+    layers = range(32)
     
-    # print(f"Starting download of {len(layers)} artifacts...")
-    # for layer in layers:
-    #     artifact_identifier = f"{entity}/{project}/{base_name}{layer}:{best_version_str}"
-    #     try:
-    #         artifact = api.artifact(artifact_identifier)
-    #         save_path = f"./"
-    #         artifact.download(root=save_path)
-    #         print(f"Successfully downloaded Layer {layer} ({best_version_str})")
-    #     except Exception as e:
-    #         # We pass silently because not all layers might be saved
-    #         print(f"Error downloading layer {layer}: {e}")
-    #         pass
+    print(f"Starting download of {len(layers)} artifacts...")
+    for layer in layers:
+        artifact_identifier = f"{entity}/{project}/{base_name}{layer}:{best_version_str}"
+        try:
+            artifact = api.artifact(artifact_identifier)
+            save_path = f"./"
+            artifact.download(root=save_path)
+            print(f"Successfully downloaded Layer {layer} ({best_version_str})")
+        except Exception as e:
+            # We pass silently because not all layers might be saved
+            print(f"Error downloading layer {layer}: {e}")
+            pass
 
     base_name = f"power-seeking-gemma3-1b-all-2-llds-0_5-{run_id}_gate-layer"
     layers = range(32)

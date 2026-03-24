84883d437ea4701f3f31f4c210db6445e7facda0
yosefnuragaw <yosefnuragaw@gmail.com>
2026-03-24 17:06:40 +0700

    feat: add llbds

--- pretrained_vector/wealth-seeking/llama-3/all-3-llbds/main.py
+++ pretrained_vector/wealth-seeking/llama-3/all-3-llbds/main.py
@@ -12,7 +12,7 @@ def download_best_bipo_step():
     
     entity = "yosefnuragaw"
     project = "Final-Wealth-Seeking"
-    run_id = "5inw2im5" 
+    run_id = "jtku87cb" 
     
     print("Fetching run history (Max 10,000 rows)...")
     run = api.run(f"{entity}/{project}/{run_id}")

dbc5f8d1dd655058fdde5204eaeec184e85df3ba
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-01 10:48:48 +0700

    feat: add prompts and ensure working generation

--- pretrained_vector/survival-instinct/mistral/all-2-llbds/main.py
+++ pretrained_vector/survival-instinct/mistral/all-2-llbds/main.py
@@ -12,7 +12,7 @@ def download_best_bipo_step():
     
     entity = "yosefnuragaw"
     project = "Final-Survival-Instinct"
-    run_id = "4cgzo3pm" 
+    run_id = "9caqefsv" 
     
     print("Fetching run history (Max 10,000 rows)...")
     run = api.run(f"{entity}/{project}/{run_id}")

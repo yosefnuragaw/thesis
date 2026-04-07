fa9d0797bbc629ba1e3b2f54e15bd6844610c1aa
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-07 20:14:03 +0700

    feat: add layer norm exp

--- models/model.py
+++ models/model.py
@@ -138,10 +138,17 @@ class BlockWrapper(torch.nn.Module):
             # Pearson Correlation
             std_product = torch.sqrt(var_h * var_v) + 1e-6
             correlation = covariance / std_product
-            abs_correlation = torch.abs(correlation)
+            # abs_correlation = torch.abs(correlation)
             
-            soft_mask =  abs_correlation
+            # 1. Buat Gerbang Biner (Lolos Threshold atau Tidak)
+            binary_gate = (correlation < 0).to(out_target.dtype)
+            
+            # --- MODIFIKASI SOFT MASKING DI SINI ---
+            # 2. Kalikan gerbang dengan nilai korelasinya
+            soft_mask =  binary_gate
 
+        # 3. Kalkulasi Injeksi
+        # 'mask' (self.multiplier) dikalikan dengan soft_mask dan vektor v
         injection = soft_mask * (mask * v)
 
         # 4. Implementasi ke dalam arsitektur

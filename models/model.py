edd3049ae1afc39e539a8c60ef2d6b3b1c7c7c92
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-08 01:13:58 +0700

    feat: add layer norm exp

--- models/model.py
+++ models/model.py
@@ -150,7 +150,9 @@ class BlockWrapper(torch.nn.Module):
             
             # 7. FINAL INJEKSI
             # Hasilnya: v_mul yang disuntikkan kekuatannya sudah diredam oleh normanya sendiri
-            final_injection = (weighted_selection.to(out_target.dtype)) * v_mul
+            # final_injection = (weighted_selection.to(out_target.dtype)) * v_mul
+            final_injection = v_weight * v_mul
+
 
         # 8. Implementasi ke dalam arsitektur
         if isinstance(output, tuple):

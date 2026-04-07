fde1e465e758bfdbaf23e4997f172ab8fdabc09b
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-08 02:37:15 +0700

    feat: add layer norm exp

--- models/model.py
+++ models/model.py
@@ -134,10 +134,10 @@ class BlockWrapper(torch.nn.Module):
             # D. Hitung Resistensi Model (Opsional tapi bagus)
             # Semakin besar energi asli model (out_norm), semakin kecil suntikannya
             out_norm = torch.norm(out_target.to(torch.float32), p=2, dim=-1, keepdim=True)
-            resistance = 1.0 / (out_norm + 1e-6)
+            out_norm_scaled = out_norm / (out_norm.mean(dim=1, keepdim=True) + 1e-6)
             
             # E. Final Gabungan
-            final_injection = ( resistance * steering_force).to(out_target.dtype)
+            final_injection = ( out_norm_scaled * steering_force).to(out_target.dtype)
                     
 
         # 8. Implementasi ke dalam arsitektur

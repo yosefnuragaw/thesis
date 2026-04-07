1795db3905b2c4936ef5e7a85e4f09034fa33e1e
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-08 02:56:56 +0700

    feat: add layer norm exp

--- models/model.py
+++ models/model.py
@@ -139,17 +139,17 @@ class BlockWrapper(torch.nn.Module):
             # E. Final Gabungan
             final_injection = ( out_norm_scaled * steering_force).to(out_target.dtype)
             
-            for x in range(30):
-                mul = out_norm_scaled * self.multiplier
-                sample = mul[x].detach() 
+            # for x in range(30):
+            #     mul = out_norm_scaled * self.multiplier
+            #     sample = mul[x].detach() 
     
-                s_max = sample.max().item()
-                s_min = sample.min().item()
-                s_mean = sample.mean().item()
-                s_diff = s_max - s_min
+            #     s_max = sample.max().item()
+            #     s_min = sample.min().item()
+            #     s_mean = sample.mean().item()
+            #     s_diff = s_max - s_min
                 
-                print(f"{x:02d}     | {s_max:.4f} | {s_mean:.4f} | {s_min:.4f} | {s_diff:.4f}")
-            raise ValueError
+            #     print(f"{x:02d}     | {s_max:.4f} | {s_mean:.4f} | {s_min:.4f} | {s_diff:.4f}")
+            # raise ValueError
                     
 
         # 8. Implementasi ke dalam arsitektur

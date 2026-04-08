89892d4691a5257219ade74a66f74c2c66283c68
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-09 05:54:31 +0700

    feat: add layer norm exp

--- models/model.py
+++ models/model.py
@@ -97,7 +97,7 @@ class BlockWrapper(torch.nn.Module):
             cos_sim = torch.nn.functional.cosine_similarity(avg_output, self.vec.detach(), dim=-1)
             cos_sim_c= torch.clamp(cos_sim, min=-1.0, max=1.0) 
             cos_dis = 1 - cos_sim_c
-            self.cosine_space.append(cos_sim_c)
+            self.cosine_space.append(cos_dis)
 
            
             

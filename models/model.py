2c45ca5069f1afb0fd6257a6b46ad44f938d8c32
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-09 02:46:00 +0700

    feat: add layer norm exp

--- models/model.py
+++ models/model.py
@@ -179,7 +179,7 @@ class BlockWrapper(torch.nn.Module):
         # You MUST clamp it so the sink tokens lock exactly at 1.0.
         soft_mask = torch.clamp((out_norm - self.cache_min) / norm_range, 0.0, 1.0)
 
-        llbds = soft_mask 
+        llbds = 1+(soft_mask-0.5) 
         print(soft_mask)
         
         v_base = self.vec.detach().to(out_target.device).to(torch.float32).view(1, 1, -1)

e2ce331f1d17b988c10d190714806b945ab95983
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-09 03:34:55 +0700

    feat: add layer norm exp

--- evaluation.py
+++ evaluation.py
@@ -317,7 +317,7 @@ if __name__ == "__main__":
             model=model,
             tokenizer=tokenizer,
             layers=script_args.layer,
-            multipliers= [10,2,-2,-10],
+            multipliers= [2,-2],
             messages=messages,
             max_new_tokens = script_args.max_new_tokens,
             temperature = script_args.temperature ,

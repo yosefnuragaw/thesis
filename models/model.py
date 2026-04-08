446d7c79a513ad431622b95bc0a0045acee78400
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-08 21:44:47 +0700

    feat: add layer norm exp

--- models/model.py
+++ models/model.py
@@ -88,7 +88,6 @@ class BlockWrapper(torch.nn.Module):
        
     def forward(self, hidden_states, *args, **kwargs):
         output = self.block(hidden_states, *args, **kwargs)
-        print(hidden_states.shape)
         with torch.no_grad():
             out_tensor = output[0] if isinstance(output, tuple) else output
             avg_hidden = hidden_states.detach().mean(dim=1)

86ad91f2292cc8f6abeb88e1e025c707a24e0295
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-09 05:38:34 +0700

    feat: add layer norm exp

--- models/model.py
+++ models/model.py
@@ -53,21 +53,11 @@ class MaskGate2(torch.nn.Module):
 
 
 class BlockWrapper(torch.nn.Module):
-    def __init__(self, 
-                 block, 
-                 hidden_dim, 
-                 vec: Optional[torch.Tensor] = None, 
-                 buffer: bool = False, 
-                 gate_function:Optional[str] = None, 
-                 skip:Optional[str] = None, 
-                 k1:float = 0.,
-                 use_cache: bool = False):
-        
+    def __init__(self, block, hidden_dim, vec: Optional[torch.Tensor] = None, buffer: bool = False, gate_function:Optional[str] = None, skip:Optional[str] = None, k1:float = 0.):
         super().__init__()
         self.multiplier = 1.0
         self.block = block
         self.k1 = k1
-        self.use_cache = use_cache
         try:
             ref_param = next(block.parameters())
             self.init_dtype = ref_param.dtype
@@ -95,17 +85,22 @@ class BlockWrapper(torch.nn.Module):
         self.buffer_space = []
         self.cosine_space = []
 
-       
     def forward(self, hidden_states, *args, **kwargs):
         output = self.block(hidden_states, *args, **kwargs)
+
         with torch.no_grad():
             out_tensor = output[0] if isinstance(output, tuple) else output
             avg_hidden = hidden_states.detach().mean(dim=1)
             avg_output = out_tensor.detach().mean(dim=1)
             cos_sim = torch.nn.functional.cosine_similarity(avg_hidden, avg_output, dim=-1)
             cos_sim_c= torch.clamp(cos_sim, min=-1.0, max=1.0) 
+            print(f'old{cos_sim_c}')
+            cos_sim = torch.nn.functional.cosine_similarity(avg_output, self.vec.detach(), dim=-1)
+            cos_sim_c= torch.clamp(cos_sim, min=-1.0, max=1.0) 
+            print(f'old{cos_sim_c}')
             cos_dis = 1 - cos_sim_c
-       
+           
+            
         mask = self.multiplier 
         
         if self.skip:
@@ -117,51 +112,15 @@ class BlockWrapper(torch.nn.Module):
             while mask.dim() < hidden_states.dim():
                 mask = mask.unsqueeze(-1)
 
-        # if isinstance(output, tuple):
-        #     self.buffer_space.append(output[0].detach().mean(dim=1).cpu())
-        #     modified_hidden = output[0] + (mask * self.vec.to(output[0].device))
-        #     output = (modified_hidden,) + output[1:]
-            
-        # elif isinstance(output, torch.Tensor):
-        #     self.buffer_space.append(output.detach().mean(dim=1).cpu())
-        #     output = output + (mask * self.vec.to(output.device))
-
-        out_target = output[0] if isinstance(output, tuple) else output
-        out_norm = torch.norm(out_target.to(torch.float32), p=2, dim=-1, keepdim=True)
-
-        with torch.no_grad():
-            
-            batch_min = out_norm.min()
-            batch_max = out_norm.max()
-            if out_target.size(1) == 1:
-                if not hasattr(self, 'cache_min') or self.cache_min is None:
-                    self.register_buffer('cache_min', batch_min)
-                    self.register_buffer('cache_max', batch_max)
-                else:
-                    self.cache_min = torch.minimum(self.cache_min, batch_min)
-                    self.cache_max = torch.maximum(self.cache_max, batch_max)
-
-                batch_min = torch.minimum(self.cache_min, batch_min)
-                batch_max = torch.maximum(self.cache_max, batch_max)
-
-        norm_range = batch_max - batch_min + 1e-6
-        soft_mask = (out_norm - batch_min) / norm_range
-        binary_mask = (out_norm == batch_max).to(out_target.dtype)
-
-        llbds = binary_mask * 10
-        final_injection = (llbds * self.multiplier * self.vec.to(out_target.device)).to(out_target.dtype)
-                    
-
-        # 8. Implementasi ke dalam arsitektur
         if isinstance(output, tuple):
-            self.buffer_space.append(out_target.detach().mean(dim=1).cpu())
-            modified_hidden = out_target + final_injection
+            self.buffer_space.append(output[0].detach().mean(dim=1).cpu())
+            modified_hidden = output[0] + (mask * self.vec.to(output[0].device))
             output = (modified_hidden,) + output[1:]
             
         elif isinstance(output, torch.Tensor):
-            self.buffer_space.append(out_target.detach().mean(dim=1).cpu())
-            output = out_target + final_injection
-
+            self.buffer_space.append(output.detach().mean(dim=1).cpu())
+            output = output + (mask * self.vec.to(output.device))
+        
         return output
 
     def set_multiplier(self, multiplier):
@@ -212,28 +171,3 @@ class BlockWrapper(torch.nn.Module):
         std_val = all_distances.std().item() if all_distances.numel() > 1 else 0.0
         
         return mean_val, std_val
-    
-
-    def apply_sparse_steering(self, hidden_states):
-        t = 0.25 
-
-        with torch.no_grad():
-            v = self.vec.to(hidden_states.device)
-            
-            mean_h = hidden_states.mean(dim=-1, keepdim=True)
-            mean_v = v.mean()
-
-            centered_h = hidden_states - mean_h
-            centered_v = v - mean_v
-            
-            # Hitung Kovarians
-            covariance = (centered_h * centered_v).mean(dim=-1, keepdim=True)
-            abs_covariance = torch.abs(covariance)
-            
-            mask = (abs_covariance >= t).to(hidden_states.dtype)
-        
-        # 2. Injeksi terarah DI LUAR no_grad
-        injection = mask * (self.multiplier * v)
-        steered_states = hidden_states + injection
-        
-        return steered_states

ada5a1dc4c02e66b045c49cf13222f51bdb0ffe3
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-09 01:08:17 +0700

    feat: add layer norm exp

--- evaluation.py
+++ evaluation.py
@@ -290,7 +290,7 @@ if __name__ == "__main__":
     )
     
     if args.task != "generation":
-        for mul in [0]:      
+        for mul in [10]:      
             template_save_path = f"activation/{script_args.model_name_or_path.split("/")[-1]}/{script_args.behavior}/{script_args.id}_buffer_{{layer}}_{{mul}}.pt" 
             accuracy = eval_accuracy(
                 model=model,

53a48e07e06828fca4b329775e399fed5bf1fefd
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-09 01:48:16 +0700

    feat: add layer norm exp

--- models/model.py
+++ models/model.py
@@ -148,7 +148,7 @@ class BlockWrapper(torch.nn.Module):
         norm_range = max_val - min_val + 1e-6
         soft_mask = (out_norm - min_val) / norm_range
 
-        llbds =  1 + (1-soft_mask - 0.5)
+        llbds =  1 + (soft_mask - 0.5)
         final_injection = (llbds * self.multiplier * self.vec.to(out_target.device)).to(out_target.dtype)
                     
 

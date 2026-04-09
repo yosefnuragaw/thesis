5304608bc1062852809088ca9fd44a00b9ef81ed
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-09 07:17:37 +0700

    feat: add layer norm exp

--- pretrained_vector/power-seeking/llama-3/16-skip/main.py
+++ pretrained_vector/power-seeking/llama-3/16-skip/main.py
@@ -12,7 +12,7 @@ def download_best_bipo_step():
     
     entity = "yosefnuragaw"
     project = "Final-Power-Seeking"
-    run_id = "fg8czgbc" 
+    run_id = "u9fnrtsw" 
     
     print("Fetching run history (Max 10,000 rows)...")
     run = api.run(f"{entity}/{project}/{run_id}")
@@ -59,7 +59,7 @@ def download_best_bipo_step():
     print(f"-------------------------\n")
 
     # 6. Download the artifacts
-    base_name = f"power-seeking-llama3-1-8b-16-sink-{run_id}_steering-vec-layer"
+    base_name = f"power-seeking-llama3-1-8b-16-skip-{run_id}_steering-vec-layer"
     layers = range(32)
     
     print(f"Starting download of {len(layers)} artifacts...")

2f45667e8f5ebd26c3123e520aaa6681b9ff5ef1
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-11 21:11:10 +0700

    feat: add rapr

--- rapr.py
+++ rapr.py
@@ -302,7 +302,7 @@ def get_prompts(
     behavior,
     system_prompt=SYSTEM_PROMPT,
     generation_prompt: bool = True,
-    k: int = 60,
+    k: int = 30,
     seed: int = 42,
 ):
     path = f"./data/{behavior}/train.csv"

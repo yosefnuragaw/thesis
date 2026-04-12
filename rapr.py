a7c91a4a5647789263ac4478ba388027f1193ed0
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-12 23:20:38 +0700

    feat: add rapr

--- rapr.py
+++ rapr.py
@@ -294,12 +294,7 @@ class RAPR(PatcherEngine):
         avg_ops_sens = np.nanmean(ops_sens_matrix_3d, axis=(0, 2))
         
         friction_vec = avg_sens - avg_ops_sens
-        # max_friction = np.max(np.abs(friction_vec)) + 1e-8
-        # norm_friction = friction_vec / max_friction # Range: [-1.0 to 1.0]
-        
-        # Positive friction (resistance) becomes a penalty (-).
-        # Negative friction (assistance) becomes a reward (+).
-        gated_friction = np.sign(friction_vec) * influence_vec * 0.5
+        gated_friction = np.sign(friction_vec) * influence_vec * 0.25
         
         # 6. Combine and cap
         influence = influence_vec + gated_friction

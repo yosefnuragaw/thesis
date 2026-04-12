8d215c13945ad76747a47c0d5755db1592a29090
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-12 08:32:39 +0700

    feat: add rapr

--- rapr.py
+++ rapr.py
@@ -653,7 +653,7 @@ if __name__ == "__main__":
         loader=loader,
         verbose=True,
     )
-    multipliers_to_test = [0.1,0.5,1.0]
+    multipliers_to_test = [0.1,0.5,1.0, 1.5, 2.0]
     print(f"\nStarting Calibration Sweep across Multipliers: {multipliers_to_test}")
 
     sweep_results = engine.compute_matrix_sweep(multipliers_to_test)

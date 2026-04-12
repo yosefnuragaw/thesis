9f63580d4c035709ad3b90fe6bf4ff64805faa7c
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-13 00:08:32 +0700

    feat: add rapr

--- models/model.py
+++ models/model.py
@@ -106,7 +106,7 @@ class BlockWrapper(torch.nn.Module):
             rel_norm = vec_norm / output_norm_safe
             self.rel_norm_space.append(rel_norm)
             
-        mask = self.multiplier * torch.clamp(cos_dis, min=.0) 
+        mask = self.multiplier * torch.clamp(cos_sim_c, min=0.) 
 
         if self.skip:
             if self.skip == 'llbds':

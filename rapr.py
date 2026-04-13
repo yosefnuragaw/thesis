c6b1ed30fed5f057433d5bebfc4ca6a3d8921e81
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-14 01:26:14 +0700

    feat: add rapr

--- rapr.py
+++ rapr.py
@@ -304,8 +304,8 @@ class RAPR(PatcherEngine):
         ]
 
         multipliers = [
-            -0.9, 
-            0.2
+            -1.0, 
+            0.
         ]
 
         # Apply the conditions, defaulting to 0.0 if friction_vec is exactly 0

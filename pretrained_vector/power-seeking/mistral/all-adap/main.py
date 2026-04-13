e2e831c228843d5661250994079824d6748c2ded
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-14 05:10:52 +0700

    feat: add rapr

--- pretrained_vector/power-seeking/mistral/all-adap/main.py
+++ pretrained_vector/power-seeking/mistral/all-adap/main.py
@@ -12,7 +12,7 @@ def download_best_bipo_step():
     
     entity = "yosefnuragaw"
     project = "Final-Power-Seeking"
-    run_id = "0mwtts2y" 
+    run_id = "ytjdkaku" 
     
     print("Fetching run history (Max 10,000 rows)...")
     run = api.run(f"{entity}/{project}/{run_id}")

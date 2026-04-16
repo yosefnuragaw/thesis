3202a5a0c3b8af0a41ac98bac4f14cf0cfe5be6c
yosefnuragaw <yosefnuragaw@gmail.com>
2026-04-16 16:03:58 +0700

    feat: add corr and surv

--- rapr.py
+++ rapr.py
@@ -304,7 +304,7 @@ class RAPR(PatcherEngine):
         
         friction_vec = avg_sens - avg_ops_sens
 
-        top_k_mask = np.where(friction_vec > 0, 1.0, 0.0)
+        top_k_mask = np.where(__norm(friction_vec) > 0, 1.0, 0.0)
 
         influence = influence_vec * top_k_mask
         

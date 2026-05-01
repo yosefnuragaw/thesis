c44a166d47d8c3a5486c05d014c755ec66faf454
yosefnuragaw <yosefnuragaw@gmail.com>
2026-05-01 23:59:59 +0700

    feat: changed sdpa

--- models/model.py
+++ models/model.py
@@ -205,7 +205,10 @@ class CAABlockWrapper(torch.nn.Module):
         if clear:
             self.caa_buffer = {'pos': [], 'neg': []}
         
-        return vec_pos, vec_neg
+
+        sv_pos = vec_pos-vec_neg
+        sv_neg = vec_neg-vec_pos
+        return sv_pos, sv_neg
 
     @override
     def forward(self, hidden_states, *args, **kwargs):

b36ab22cee6fe712cd94cad7d53ffc27835c3e04
yosefnuragaw <yosefnuragaw@gmail.com>
2026-05-06 09:39:22 +0700

    feat: add coor

--- evaluation_caa.py
+++ evaluation_caa.py
@@ -5,7 +5,6 @@ from transformers import (
     HfArgumentParser
 )
 from dataclasses import dataclass, field
-import argparse
 import torch
 import os
 from typing import Any, List, Optional
@@ -15,8 +14,45 @@ from torch.utils.data import DataLoader
 
 from models.dataset import SteerbenchDataset
 from models.model import CAABlockWrapper
-from utils import set_seed, get_sterbench_data, batch_logps
+from utils import set_seed, load_dataset,batch_logps
+SYSTEM_PROMPT= "You are a helpful, honest and concise assistant."
 
+def get_sterbench_data(tokenizer, behavior, system_prompt=SYSTEM_PROMPT, generation_prompt:bool = True):
+    path = f"./data/{behavior}/test_infer.csv"
+    if not os.path.exists(path):
+         raise FileNotFoundError(f"Data file not found: {path}")
+         
+    dataset = load_dataset("csv", data_files=path, split='train')
+    
+    questions = [] 
+    prompts = []  
+    labels = []    
+    communities=[]
+    for row in dataset:
+        if row['question'] is not None:  
+            messages = [
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": row['question']},
+            ]
+            full_prompt = tokenizer.apply_chat_template(
+                messages, 
+                tokenize=False, 
+                add_generation_prompt=generation_prompt
+            )
+            
+            questions.append(full_prompt)
+            
+            current_options = [row[col] for col in ['A','B','C','D'] if col in row and row[col] is not None]
+            prompts.append(current_options)
+            labels.append(row['label'])
+            communities.append(row['communities'])
+
+    return{
+        'questions':questions,
+        'prompts':prompts,
+        'labels':labels,
+        'communities':communities,
+    }
 @dataclass
 class ScriptArguments:
     id: Optional[str] = field(default="baseline", metadata={"help": "Run id"})
@@ -54,14 +90,13 @@ def init_model(
 
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
-        # attn_implementation="flash_attention_2",
         attn_implementation="sdpa",
-        low_cpu_mem_usage=True,
         trust_remote_code=True,
     )
     model.warnings_issued = {}
     model.to("cuda" if torch.cuda.is_available() else "cpu")
     space = []
+    
     for layer in range(total_layer):
         if baseline:
             vec = torch.zeros(model.config.hidden_size, dtype=model.dtype)
@@ -69,21 +104,23 @@ def init_model(
             vec_path = os.path.join(vec_dir, f"pos_layer_{layer}.pt")
             neg_path = os.path.join(vec_dir, f"neg_layer_{layer}.pt")
             
-            
             if os.path.exists(vec_path) and os.path.exists(neg_path):
                 space.append([
                     torch.load(vec_path, map_location="cuda"), 
                     torch.load(neg_path, map_location="cuda")
                 ])
             else:
-                space.append([torch.zeros(model.config.hidden_size, dtype=model.dtype), torch.zeros(model.config.hidden_size, dtype=model.dtype)])
+                space.append([
+                    torch.zeros(model.config.hidden_size, dtype=model.dtype), 
+                    torch.zeros(model.config.hidden_size, dtype=model.dtype)
+                ])
 
             model.model.layers[layer] = CAABlockWrapper(
-            model.model.layers[layer],
-            hidden_dim=model.config.hidden_size,
-            vec = torch.zeros(model.config.hidden_size, dtype=model.dtype),
-            apply_type=apply_type,
-            treshold = treshold
+                model.model.layers[layer],
+                hidden_dim=model.config.hidden_size,
+                vec = torch.zeros(model.config.hidden_size, dtype=model.dtype),
+                apply_type=apply_type,
+                treshold = treshold
             )
             model.model.layers[layer].extract(False)  
 
@@ -96,7 +133,7 @@ def init_model(
     return model, tokenizer, space
 
 def produce_dataloader(behavior: str, tokenizer: AutoTokenizer):
-    data = get_sterbench_data(tokenizer = tokenizer, behavior= behavior)
+    data = get_sterbench_data(tokenizer=tokenizer, behavior=behavior)
 
     eval_dataset = SteerbenchDataset(
         tokenizer=tokenizer,
@@ -114,9 +151,7 @@ def produce_dataloader(behavior: str, tokenizer: AutoTokenizer):
     ) 
     return eval_loader
 
-def eval_accuracy(
-        model, loader: DataLoader, multiplier: float, layers: List[int], vecs
-    ):
+def eval_accuracy(model, loader: DataLoader, multiplier: float, layers: List[int], vecs):
     OPT = ['A', 'B', 'C','D']
     correct = 0
     total = 0
@@ -124,9 +159,9 @@ def eval_accuracy(
     for batch in loader:
         label = batch["label"][0]
         q_len = batch["question_length"]
-        comm = batch['communities']
-
-        vec_direc = 1 if comm == 'r/A' else 'r/B'
+        
+        comm = batch['communities'][0]
+        vec_direc = 0 if comm == 'r/A' else 1
 
         for layer in layers:
             if isinstance(model.model.layers[layer], CAABlockWrapper):
@@ -134,7 +169,7 @@ def eval_accuracy(
                 model.model.layers[layer].set_multiplier(multiplier)
 
         avg_logp = []
-        for input_ids, attention_mask, decoded in zip(batch["input_ids"], batch["attention_mask"], batch['decode']):
+        for input_ids, attention_mask in zip(batch["input_ids"], batch["attention_mask"]):
             input_ids = input_ids.to(model.device)
             attention_mask = attention_mask.to(model.device)
     
@@ -151,55 +186,56 @@ def eval_accuracy(
         if pred == label:
             correct += 1
     
-        
-        acc = correct / total 
-
+    acc = correct / total 
     print(f"[Accuracy:] {acc}")
     return acc
     
+# === EXECUTION BLOCK FOR JUPYTER ===
+CONFIG_PATH = ["/kaggle/input/notebooks/diansss/git-loader/thesis/configs/caa/environment-climateskeptics/llama3_1-8b/environment-climateskeptics-all-sequence.yaml",
+              "/kaggle/input/notebooks/diansss/git-loader/thesis/configs/caa/environment-climateskeptics/llama3_1-8b/environment-climateskeptics-all.yaml",
+              "/kaggle/input/notebooks/diansss/git-loader/thesis/configs/caa/simpleliving-unethical/llama3_1-8b/simpleliving-unethical-all-sequence.yaml",
+              "/kaggle/input/notebooks/diansss/git-loader/thesis/configs/caa/simpleliving-unethical/llama3_1-8b/simpleliving-unethical-all.yaml",
+              "/kaggle/input/notebooks/diansss/git-loader/thesis/configs/caa/liberal-conservatives/llama3_1-8b/liberal-conservatives-all-sequence.yaml",
+              "/kaggle/input/notebooks/diansss/git-loader/thesis/configs/caa/liberal-conservatives/llama3_1-8b/liberal-conservatives-all.yaml"]
+IS_BASELINE = False  # Set to True if you want to run the baseline evaluation
 
 if __name__ == "__main__":
     set_seed(seed=42)
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", "-c", type=str, required=True, help="Path to your YAML config file")
-    parser.add_argument("--verbose", "-v", type=bool, required=False, default=True, help="Visualize eval progress")
-    parser.add_argument("--baseline", action='store_true')
 
-    args, remaining = parser.parse_known_args()
-
-    hf_parser = HfArgumentParser(ScriptArguments)
-    if args.config.endswith(".yaml"):
-        script_args = hf_parser.parse_yaml_file(yaml_file=args.config, allow_extra_keys=True)[0]
-    elif args.config.endswith(".json"):
-        script_args = hf_parser.parse_json_file(json_file=args.config, allow_extra_keys=True)[0]
-    else:
-        raise ValueError("Config file must be .yaml or .json")
+    for conf in CONFIG_PATH:
+        # Parse the YAML configuration directly
+        print(conf)
+        hf_parser = HfArgumentParser(ScriptArguments)
+        if conf.endswith(".yaml"):
+            script_args = hf_parser.parse_yaml_file(yaml_file=conf, allow_extra_keys=True)[0]
+        elif conf.endswith(".json"):
+            script_args = hf_parser.parse_json_file(json_file=conf, allow_extra_keys=True)[0]
+        else:
+            raise ValueError("Config file must be .yaml or .json")
+        
+        print(f"Loading model: {script_args.model_name_or_path}...")
+        model, tokenizer, vecs = init_model(
+            model_name=script_args.model_name_or_path,
+            vec_dir=script_args.vec_dir,
+            layers=script_args.layer,
+            apply_type=script_args.apply_type,
+            total_layer=script_args.total_layer,
+            baseline=IS_BASELINE,
+            treshold=script_args.treshold
+        )
     
-    model, tokenizer, vecs = init_model(
-        model_name=script_args.model_name_or_path,
-        vec_dir=script_args.vec_dir,
-        layers=script_args.layer,
-        apply_type=script_args.apply_type,
-        total_layer=script_args.total_layer,
-        baseline=args.baseline,
-        treshold = script_args.treshold
-    )
-
-    eval_loader = produce_dataloader(
-        behavior=script_args.behavior,
-        tokenizer=tokenizer
-    )
-    
-    for mul in [1]:            
-        accuracy = eval_accuracy(
+        print("Generating dataloader...")
+        eval_loader = produce_dataloader(
+            behavior=script_args.behavior,
+            tokenizer=tokenizer
+        )
+        
+        print("Starting evaluation...")
+        for mul in [1]:            
+            accuracy = eval_accuracy(
                 model=model,
                 loader=eval_loader,
                 multiplier=mul,
                 layers=script_args.layer, 
                 vecs=vecs,
-            ) 
-
-    
-
-
- 
+            )
